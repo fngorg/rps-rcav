c660a59ade537f7d463862e2c46b2d52924ede4e
francisco navarro-grau <fnavarrograu@chicagobooth.edu>
2023-04-12 16:26:42 +0000

    final commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,16 @@
 class ApplicationController < ActionController::Base
-  layout(false)
+  layout("wrapper.html.erb")
 
   # Add your actions below this line
   # ================================
 
   def homepage
-
     render({ :template => "game_templates/rules.html.erb"})
-
   end
 
 
   def play_rock
-
    # redirect_to("https://www.wikipedia.org")
-
    # render({:html => "<h1>Howdy, world!</h1>".html_safe})
 
    render({:template => "game_templates/user_rock.html.erb"})
